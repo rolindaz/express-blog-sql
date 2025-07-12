ba2052295f985ee0e3a4eac041e7c67da2040b9c
rolindaz <rolinda.zamboni@gmail.com>
2025-07-12 17:09:57 +0200

    M2 - Add error handling middleware to postController INDEX route

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -2,7 +2,8 @@ function errorsHandler(err, req, res, next) {
 	console.log('Something is wrong on the server side');
 	res.status(500);
 	res.json({
-		error: err.message
+		error: true,
+		message: err.message
 	});
 };
 
